3f2ead66da99ed63413a7fba1f13ff9e3e7199e3
Sævar Öfjörð Magnússon <saevar@overcast.is>
2021-05-11 11:21:36 +0000

    3D Secure

--- saltpay_python/saltpay.py
+++ saltpay_python/saltpay.py
@@ -57,7 +57,7 @@ class SaltpayClient(object):
 
         return self.make_request("/api/token/multi", "POST", json=payload)
 
-    def Payment(self, Token, Amount, Currency, OrderId):
+    def Payment(self, Token, Amount, Currency, OrderId, MpiToken=None):
 
         assert str(Currency) in ISO4217Numeric
 
@@ -68,12 +68,74 @@ class SaltpayClient(object):
             "Amount": Amount,
             "Currency": Currency,
             "TransactionDate": TransactionDate,
-            "OrderId": "WHATTHEHELL1",
+            "OrderId": OrderId,
             "PaymentMethod": {
                 "PaymentType": "TokenMulti",
                 "Token": Token,
             }
-        }        
+        }
+        if MpiToken:
+            payload["ThreeDSecure"] = {
+                "DataType": "Token",
+                "MpiToken": MpiToken,
+            }
 
         return self.make_request("/api/payment", "POST", json=payload)
 
+
+    def Enrollment(self, PAN, ExpMonth, ExpYear, Amount, Exponent, Currency, CVC2, OkUrl, FailUrl, TermUrl, MD=''):
+        payload = {
+            "CardDetails": {
+                "PaymentType": "Card",
+                "PAN": PAN,
+                "ExpYear": ExpYear,
+                "ExpMonth": ExpMonth,
+                "CVC2": CVC2,
+            },
+            "Amount": Amount,
+            "Exponent": Exponent,
+            "Currency": Currency,
+            "MD": base64.b64encode(MD.encode()).decode('utf-8'),
+            "OkUrl": OkUrl,
+            "FailUrl": FailUrl,
+            "TermUrl": TermUrl,
+        }
+
+        response = self.make_request("/api/mpi/v2/enrollment", "POST", json=payload)
+
+        if response["ResultStatus"] == 0 and response["MdStatus"] == "9":
+            url = ""
+            fields = {}
+            for item in response["RedirectToACSData"]:
+                if item["Name"] == "actionURL":
+                    url = item["Value"]
+                else:
+                    fields[item["Name"]] = item["Value"]
+
+            return {
+                'token': response['MPIToken'],
+                'postUrl': url,
+                'verificationFields': fields,
+                'html': response['RedirectToACSForm']
+            }
+        raise SaltPayException(message=f"Unsuccessful request. {response['MdErrorMessage']}")
+
+
+    def Validation(self, PARes=None, CRes=None):
+
+        if PARes or (PARes and CERes):
+
+            payload = {
+                "PARes": PARes,
+            }
+        if not PARes and CRes:
+            payload = {
+                "CRes": CRes
+            }
+
+        response = self.make_request("/api/mpi/v2/validation", "POST", json=payload)
+
+        if response['MdStatus'] == '1':
+            return True
+        
+        raise SaltPayException(message=f"Unsuccessful request. {response['MdErrorMessage']}")
